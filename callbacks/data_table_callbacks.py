e1fbfebca06d979a29ab1001d74345bb4b1c414f
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-04-27 00:58:13 +0200

    Improve data table loading speed performance

--- callbacks/data_table_callbacks.py
+++ callbacks/data_table_callbacks.py
@@ -3,9 +3,8 @@ from dash import Input, Output
 
 def data_table_callbacks(app):
     @app.callback(
-        Output("data-table", "data", allow_duplicate=True),
+        Output("data-table", "data"),
         Input("memory-output", "data"),
-        prevent_initial_call=True,
     )
     def populate_data_table(data):
         if data is None:
