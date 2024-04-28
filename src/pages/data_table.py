f35048ad86d3f1f06a0c320e928ef13f11c03a72
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-04-28 15:20:50 +0200

    Update page length in data table

--- src/pages/data_table.py
+++ src/pages/data_table.py
@@ -15,10 +15,10 @@ layout = dbc.Container(
                 html.H2("Data Table"),
                 html.P("Preview and manipulate the Superstore data table below."),
                 # Filter from the filter component for the data table
-                # create_data_table_filters(),
+                create_data_table_filters(),
                 dash_table.DataTable(
                     id="data-table",
-                    page_size=5,
+                    page_size=10,
                     style_table={"maxWidth": "100%", "overflowX": "auto"},
                     fill_width=True,
                 ),
