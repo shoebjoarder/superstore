5de34fa411ddc779ea89e43e8884e6b5d7ba7ef1
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-04-28 12:20:33 +0200

    Fix data loading in table

--- src/callbacks/data_table_callbacks.py
+++ src/callbacks/data_table_callbacks.py
@@ -5,7 +5,6 @@ def data_table_callbacks(app):
     @app.callback(
         Output("data-table", "data"),
         Input("memory-output", "data"),
-        prevent_initial_call=True,
     )
     def populate_data_table(data):
         if data is None:
