32364038c1acbf7fd456001330b7bcb0d41e4c27
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-04-30 07:43:51 +0200

    Add types to data table callback

--- src/callbacks/data_table_callbacks.py
+++ src/callbacks/data_table_callbacks.py
@@ -1,12 +1,13 @@
 from dash import Input, Output
+from typing import Any, Dict, List
 
 
-def data_table_callbacks(app):
+def data_table_callbacks(app: Any) -> None:
     @app.callback(
         Output("data-table", "data"),
         Input("memory-output", "data"),
     )
-    def populate_data_table(data):
+    def populate_data_table(data: Dict[str, Any]) -> List[Dict[str, Any]]:
         if data is None:
             return []
         else:
