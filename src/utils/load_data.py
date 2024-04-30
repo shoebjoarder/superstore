b328709a3d028ddc4bcf2ba6ab73b4dd381ef426
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-04-30 17:50:58 +0200

    Update the loading data function to improve readability

--- src/utils/load_data.py
+++ src/utils/load_data.py
@@ -1,8 +1,25 @@
 import pandas as pd
 import os
+from typing import Optional
+import logging
+
+# Define constants for column names and messages
+COLUMN_ORDER_ID = "Order ID"
+COLUMN_RETURNED = "Returned"
+MESSAGE_LOCAL_FILE = "***** Local Excel file used to load dataset! *****"
+MESSAGE_GITHUB_FILE = "***** Fetched the Excel file from GitHub successfully! *****"
 
 
-def load_dataset(url=None):
+def load_dataset(url: Optional[str] = None) -> pd.DataFrame:
+    """
+    Load and merge the Orders and Returns datasets from an Excel file.
+
+    Parameters:
+    - url (Optional[str]): URL to fetch the Excel file from. If None, the local file is used.
+
+    Returns:
+    - pd.DataFrame: The merged DataFrame.
+    """
     df_orders = None
     df_returns = None
     if url is None:
@@ -12,15 +29,15 @@ def load_dataset(url=None):
 
         df_orders = pd.read_excel(excel_file_path, sheet_name="Orders")
         df_returns = pd.read_excel(excel_file_path, sheet_name="Returns")
-        print("***** Local Excel file used to load dataset! *****")
+        logging.info(MESSAGE_LOCAL_FILE)
     else:
         df_orders = pd.read_excel(url, sheet_name="Orders")
         df_returns = pd.read_excel(url, sheet_name="Returns")
-        print("***** Fetched the Excel file from GitHub successfully! *****")
+        logging.info(MESSAGE_GITHUB_FILE)
 
-    merged_df = pd.merge(df_orders, df_returns, on="Order ID", how="outer")
+    merged_df = pd.merge(df_orders, df_returns, on=COLUMN_ORDER_ID, how="outer")
 
-    merged_df["Returned"] = merged_df["Returned"].fillna("No")
+    merged_df[COLUMN_RETURNED] = merged_df[COLUMN_RETURNED].fillna("No")
 
     merged_df["Order Date"] = merged_df["Order Date"].dt.strftime("%Y-%m-%d")
     merged_df["Ship Date"] = merged_df["Ship Date"].dt.strftime("%Y-%m-%d")
